4f448132037971d1f429ec4b5ae530138de7f399
AndreAAV <andreyAAV81@gmail.com>
2021-12-20 21:22:34 +0300

    new

--- src/main/java/by/itclass/config/SecurityConfig.java
+++ src/main/java/by/itclass/config/SecurityConfig.java
@@ -1,12 +1,83 @@
 package by.itclass.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    protected void configure(HttpSecurity http) throws Exception {
+        //метод antMatchers() позволяет настроить безопасность на определенные ресурсы приложения
+        http.authorizeRequests()
+                .antMatchers("/admin.html").hasAuthority("ADMIN")
+                .antMatchers("/panel.html").hasRole("DEFAULT")
+                .antMatchers("/main.html", "/main")
+                .authenticated()
+                .and()
+                .formLogin();
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager service = new JdbcUserDetailsManager(dataSource);
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .authorities("USER", "ADMIN")
+                .build();
+
+
+
+        if (!service.userExists(admin.getUsername())) {
+            service.createUser(admin);
+        }
+
+        return service;
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password("{bcrypt}$2a$12$Tpx48FFcFToBo/TqPWqILekhbyB1Fbh3Et9zCW2BS.6nyp9YN2C3C")
+//                .authorities("USER")
+//                .roles("DEFAULT")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password("{bcrypt}$2a$12$KQSPAJNVHSG34KX4Uf6DiO53U0qJ53j.HrRCe.CzuA0RTHcYgPUSC")
+//                .authorities("USER", "ADMIN")
+//                .roles("DEFAULT")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new JdbcUserDetailsManager();
+//    }
 
 
 }
--- src/main/java/by/itclass/controllers/MainController.java
+++ src/main/java/by/itclass/controllers/MainController.java
@@ -1,13 +1,16 @@
 package by.itclass.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/main")
-    public String main() {
+    public String main(Authentication authentication) {
         return "main.html";
     }
 }
